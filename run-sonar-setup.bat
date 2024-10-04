ee1e27541be43d739d8fb330b96ef03322a1279b
mrbiglari <47533779+mrbiglari@users.noreply.github.com>
2024-10-04 09:12:28 +0100

    feat: Hiding printing of .bat command lines and adding echos for when a dependency is already installed.

--- run-sonar-setup.bat
+++ run-sonar-setup.bat
@@ -1,3 +1,5 @@
+@echo off
+
 :: Dependencies for dotnet
 call :check_dotnet_coverage
 call :check_dotnet_sonarscanner
@@ -5,17 +7,21 @@ call :check_dotnet_sonarscanner
 :: Dependencies for dotnet framework
 call :check_dotnet_coverlet
 
+echo All dependencies checked and installed if necessary.
+goto :eof
 
 :check_dotnet_sonarscanner
 :: Check if dotnet-sonarscanner is installed, and install if not
 dotnet tool list -g | findstr /I "dotnet-sonarscanner" >nul
 if errorlevel 1 (
-    echo "dotnet-sonarscanner is not installed. Installing now..."
+    echo dotnet-sonarscanner is not installed. Installing now...
     dotnet tool install --global dotnet-sonarscanner
     if errorlevel 1 (
-        echo "Error: Failed to install dotnet-sonarscanner."
+        echo Error: Failed to install dotnet-sonarscanner.
         exit /b 1
     )
+) else (
+    echo dotnet-sonarscanner is already installed.
 )
 goto :eof
 
@@ -23,25 +29,29 @@ goto :eof
 :: Check if dotnet-coverage is installed, and install if not
 dotnet tool list -g | findstr /I "dotnet-coverage" >nul
 if errorlevel 1 (
-    echo "dotnet-coverage is not installed. Installing now..."
+    echo dotnet-coverage is not installed. Installing now...
     dotnet tool install --global dotnet-coverage
     if errorlevel 1 (
-        echo "Error: Failed to install dotnet-coverage."
+        echo Error: Failed to install dotnet-coverage.
         exit /b 1
     )
+) else (
+    echo dotnet-coverage is already installed.
 )
 goto :eof
 
 :check_dotnet_coverlet
 :: Check if Coverlet is installed
-dotnet tool list -g | findstr "coverlet.console" >nul
+dotnet tool list -g | findstr /I "coverlet.console" >nul
 if errorlevel 1 (
-    echo "Coverlet is not installed. Installing Coverlet..."
+    echo Coverlet is not installed. Installing Coverlet...
     dotnet tool install --global coverlet.console
     if errorlevel 1 (
-        echo "Error: Failed to install Coverlet."
+        echo Error: Failed to install Coverlet.
         exit /b 1
     )
-    echo "Coverlet installed successfully."
+    echo Coverlet installed successfully.
+) else (
+    echo Coverlet is already installed.
 )
 goto :eof
